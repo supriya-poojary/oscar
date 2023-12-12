b32ea2435a2bb0da2e998373ececa5bedff7500d
supriya-poojary <153600638+supriya-poojary@users.noreply.github.com>
2023-12-12 14:04:35 +0530

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"/>
+   <link rel="stylesheet" href="css\aboutme.css">
+   <title>Contact us</title>
+
+
+</head>
+<body>
+  <div class="heading" >
+      <h1>Our Team</h1>
+  </div>
+
+    <div class="container">
+
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h4>Vinyas</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">
+                    <a href="https://instagram.com/vinyas_24?igshid=MzMyNGUyNmU2YQ=="><i class="fa fa-instagram" aria-hidden="true"></i></a>    
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                 <div class="contentBx">
+                    <h4>Swayam</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">  
+                    <a href="https://instagram.com/_swayam09?igshid=MzMyNGUyNmU2YQ=="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h4>Supriya</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">  
+                    <a href="https://instagram.com/_supriyaa019.____?igshid=MzMyNGUyNmU2YQ=="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h4>Shreesha</h4>
+                    <h5>Student</h5>
+                </div>
+                <div class="sci">   
+                    <a href="https://instagram.com/shreesha.acharya.12?igshid=MzMyNGUyNmU2YQ=="><i class="fa fa-instagram" aria-hidden="true"></i></a>   
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
